690a1de2ed20d7471c8e36212e10077e8b0f6bfd
Oleksii Bolotin <samkiral73@gmail.com>
2023-04-11 22:53:05 +0200

    finish

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,13 +1,21 @@
 import { Button } from "components/Button/Button";
 import css from "./TaskForm.module.css";
 
+import { useDispatch } from "react-redux";
+// Импортируем генератор экшена
+import { addTask } from "../../redux/actions";
 export const TaskForm = () => {
+  // Получаем ссылку на функцию отправки экшенов
+  const dispatch = useDispatch();
+  console.log(dispatch);
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
+    // Вызываем генератор экшена и передаем текст задачи для поля payload
+    // Отправляем результат - экшен создания задачи
+    dispatch(addTask(form.elements.text.value));
     form.reset();
   };
-
   return (
     <form className={css.form} onSubmit={handleSubmit}>
       <input
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { App } from "components/App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import { store } from "./redux/store";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
